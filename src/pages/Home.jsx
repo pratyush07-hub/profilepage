05547b1dc541674b06e2f7d3b65dc79b14e169ba
Pratyush <choudharypratyush809@gmail.com>
2025-04-13 21:52:07 +0530

    mobile responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,21 +15,21 @@ const Home = () => {
           ></div>
         </div>
 
-        <div className="w-full absolute md:relative md:w-[55%] flex items-center">
-          <div className="p-6 pt-110 md:p-10">
+        <div className="w-full absolute bottom-0 md:relative md:w-[55%] flex items-center">
+          <div className="p-6 md:p-10">
             <span className="text-xl md:text-4xl block text-white md:text-blue-900 font-semibold">
               Hi, I am
             </span>
-            <span className="block pt-2 text-4xl md:text-6xl font-bold text-white md:text-purple-500">
+            <span className="block pt-0 md:pt-2 text-4xl md:text-6xl font-bold text-white md:text-purple-500">
               Aadya Mathur
             </span>
-            <p className="pt-6 text-md md:text-xl text-white md:text-blue-900">
+            <p className="pt-2 md:pt-6 text-md md:text-xl text-white md:text-blue-900">
               I'm an enthusiastic Application Developer with a strong foundation
               in frontend technologies like HTML, CSS, JavaScript, and React.
               Currently diving deeper into backend development to become a
               well-rounded full-stack developer.
             </p>
-            <ul className="list-disc list-inside text-md md:text-2xl text-white md:text-blue-900 font-semibold pt-4">
+            <ul className="list-disc list-inside text-md md:text-2xl text-white md:text-blue-900 font-semibold pt-2 md:pt-4">
               <li>I enjoy playing chess.</li>
               <li>I play BGMI in my free time.</li>
               <li>I enjoy traveling new places.</li>
